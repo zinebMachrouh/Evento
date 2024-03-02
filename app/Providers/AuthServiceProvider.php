aab4748f522710acb88be6d8793a82aea9d400bd
zinebMachrouh <zinebmac49@gmail.com>
2024-03-02 15:49:00 +0100

    Define Roles and Guards

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -19,8 +21,20 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define('is-admin', function ($user) {
+            return $user->role->name === 'admin';
+        });
+
+        Gate::define('is-organizer', function ($user) {
+            return $user->role->name === 'organizer';
+        });
+
+        Gate::define('is-client', function ($user) {
+            return $user->role->name === 'client';
+        });
     }
 }
